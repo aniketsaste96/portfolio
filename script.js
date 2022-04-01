37550b0ceb070148b17b0c3310297b62ad494290
Aniket <aniketsaste96@gmail.com>
2022-04-01 10:18:01 +0530

    cursor animation

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const swup = new Swup(); // only this line when included with script tag
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
